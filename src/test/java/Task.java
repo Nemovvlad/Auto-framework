56efa22a0326b5ce45aa27c1f6416c58f0b0769d
Vladyslav Niemov <nemovvlad.ru@gmail.com>
2022-09-21 15:23:57 +0300

    Add Task

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -44,23 +44,23 @@ public class Task {
         System.out.println("max " + max);
     }
 
-//    @Test
-//    public void task() {
-//        for (int i = 2; i <= 20; i++) {
-//            boolean isPrime = true;
-//
-//            for (int j = 2; j < i; j++) {
-//                if (i % j == 0) {
-//                    isPrime = false;
-//                    break;
-//                }
-//            }
-//
-//            if (isPrime) {
-//                System.out.print(i + " ");
-//            }
-//        }
-//    }
+    @Test
+    public void task() {
+        for (int i = 2; i <= 20; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 
 }
 
